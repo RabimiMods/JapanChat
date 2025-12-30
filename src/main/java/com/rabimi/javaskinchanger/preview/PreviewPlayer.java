c440e858611da2a62c41b2a419d60638b25fb7d6
RabimiYT <fakerabimi@gmail.com>
2025-12-31 02:15:15 +0900

    PreviewPlayer.java の更新

--- src/main/java/com/rabimi/javaskinchanger/preview/PreviewPlayer.java
+++ src/main/java/com/rabimi/javaskinchanger/preview/PreviewPlayer.java
@@ -1,5 +1,6 @@
 package com.rabimi.javaskinchanger.preview;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.Identifier;
