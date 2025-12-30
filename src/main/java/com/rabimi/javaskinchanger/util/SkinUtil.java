d9d63d4f9d31cdd890b093006f19290975357ce1
RabimiYT <fakerabimi@gmail.com>
2025-12-31 02:14:05 +0900

    SkinUtil.java の更新

--- src/main/java/com/rabimi/javaskinchanger/util/SkinUtil.java
+++ src/main/java/com/rabimi/javaskinchanger/util/SkinUtil.java
@@ -10,7 +10,7 @@ public class SkinUtil {
 
         for (int x = 0; x < 64; x++) {
             for (int y = 0; y < 64; y++) {
-                int rgb = src.getColor(x * src.getWidth() / 64, y * src.getHeight() / 64);
+                int rgb = src.getPixelColor(x, y,);
                 resized.setColor(x, y, rgb);
             }
         }
