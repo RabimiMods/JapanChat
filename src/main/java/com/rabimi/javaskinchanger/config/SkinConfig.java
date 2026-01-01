97c081d09f87d76ae97b16486ad5070772d3a391
RabimiYT <fakerabimi@gmail.com>
2026-01-01 19:03:58 +0900

    SkinConfig.java の作成

---
+++ src/main/java/com/rabimi/javaskinchanger/config/SkinConfig.java
@@ -0,0 +1,8 @@
+package com.rabimi.javaskinchanger.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SkinConfig {
+    public List<String> skinPaths = new ArrayList<>();
+}
