2c75df9afe840d1a2ecb9cbe5b76555c052f89fa
RabimiYT <fakerabimi@gmail.com>
2026-01-01 18:58:42 +0900

    FileDialogHelper.java の更新

--- src/main/java/com/rabimi/javaskinchanger/FileDialogHelper.java
+++ src/main/java/com/rabimi/javaskinchanger/FileDialogHelper.java
@@ -1,28 +1,25 @@
-package com.rabimi.javaskinchanger;
+public static void open(String title, Consumer<String> callback) {
+    MinecraftClient.getInstance().executeAsync(() -> {
+        // StringをByteBufferに変換
+        var titleBuf = MemoryUtil.memUTF8(title);
+        var filterDescription = MemoryUtil.memUTF8("PNG Image");
+        var filterBuf = MemoryUtil.memAllocPointer(1);
+        filterBuf.put(MemoryUtil.memUTF8("*.png")).flip();
 
-import net.minecraft.client.MinecraftClient;
-import org.lwjgl.util.tinyfd.TinyFileDialogs;
+        String path = TinyFileDialogs.tinyfd_openFileDialog(
+                titleBuf,
+                null,
+                filterBuf,
+                filterDescription,
+                false
+        );
 
-import java.util.function.Consumer;
+        // メモリ解放（重要）
+        MemoryUtil.memFree(filterBuf.get(0));
+        MemoryUtil.memFree(filterBuf);
+        MemoryUtil.memFree(titleBuf);
+        MemoryUtil.memFree(filterDescription);
 
-public class FileDialogHelper {
-
-    /**
-     * TinyFileDialogs によるクロスプラットフォームファイル選択
-     */
-    public static void open(String title, Consumer<String> callback) {
-
-        MinecraftClient.getInstance().executeAsync(() -> {
-
-            String path = TinyFileDialogs.tinyfd_openFileDialog(
-                    title,
-                    "",
-                    new String[]{"*.png"},
-                    "PNG Image",
-                    false
-            );
-
-            callback.accept(path);
-        });
-    }
+        callback.accept(path);
+    });
 }
