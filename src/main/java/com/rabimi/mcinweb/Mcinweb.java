01cba4c119ba767612e7d622e4b4f78e019a6ec8
RabimiYT <fakerabimi@gmail.com>
2026-03-10 22:14:56 +0900

    Refactor key binding registration for clarity

--- src/main/java/com/rabimi/mcinweb/Mcinweb.java
+++ src/main/java/com/rabimi/mcinweb/Mcinweb.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil; // これが必要です
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,18 +19,17 @@ public class Mcinweb implements ModInitializer {
     public void onInitialize() {
         LOGGER.info("MCInWeb initializing...");
 
-        // 1.21.1 対応の3引数コンストラクタ
-        // 引数: 翻訳キー, キーコード, カテゴリー翻訳キー
+        // 引数の型を明示的に指定して、コンパイラの迷いをなくします
         openBrowserKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.mcinweb.open", 
-                GLFW.GLFW_KEY_B, 
-                "category.mcinweb"
+                "key.mcinweb.open",             // 翻訳キー (String)
+                InputUtil.Type.KEYSYM,          // 入力タイプ (Type)
+                GLFW.GLFW_KEY_B,                // キーコード (int)
+                "category.mcinweb"              // カテゴリー (String)
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (openBrowserKey.wasPressed()) {
                 LOGGER.info("Browser key pressed!");
-                // ここで本来は Screen を開く
             }
         });
     }
