152447037964e8a350235d8ac6b932ba165619dc
RabimiYT <fakerabimi@gmail.com>
2026-03-10 22:20:21 +0900

    Update Mcinweb.java

--- src/main/java/com/rabimi/mcinweb/Mcinweb.java
+++ src/main/java/com/rabimi/mcinweb/Mcinweb.java
@@ -1,6 +1,6 @@
 package com.rabimi.mcinweb;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
@@ -9,24 +9,26 @@ import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Mcinweb implements ModInitializer {
+public class Mcinweb implements ClientModInitializer {
     public static final String MOD_ID = "mcinweb";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     private static KeyBinding openBrowserKey;
 
     @Override
-    public void onInitialize() {
-        LOGGER.info("MCInWeb initializing...");
+    public void onInitializeClient() {
+        LOGGER.info("MCInWeb initializing on client...");
 
-        // 最も互換性の高い 3つの引数のコンストラクタを使用
-        // (翻訳キー, キーコード, カテゴリーキー)
-        // 引数が3つの場合、Loom 1.13系はこれを String カテゴリーとして受け取ります
+        // 1.21.1で最も安全なキーバインド登録方法
         openBrowserKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.mcinweb.open", 
-                GLFW.GLFW_KEY_B, 
-                "key.categories.misc" // 独自のカテゴリーでエラーが出るため、一旦「その他」を使用
+                "key.mcinweb.open",             // 翻訳キー
+                InputUtil.Type.KEYSYM,          // 入力タイプ
+                GLFW.GLFW_KEY_B,                // デフォルトキー
+                "category.mcinweb"              // カテゴリー (Stringで通らない場合は下の補足参照)
         ));
+
+        // もし上のコードでまた Category エラーが出る場合は、最後の引数を以下に書き換えてください：
+        // KeyBinding.MISC_CATEGORY
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (openBrowserKey.wasPressed()) {
