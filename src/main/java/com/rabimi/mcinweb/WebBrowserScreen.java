39eba0be140e7bf9ed9cd8f06044b553204ce6f4
RabimiYT <fakerabimi@gmail.com>
2026-03-11 15:07:11 +0900

    Replace class_2960 with Identifier in WebBrowserScreen

--- src/main/java/com/rabimi/mcinweb/WebBrowserScreen.java
+++ src/main/java/com/rabimi/mcinweb/WebBrowserScreen.java
@@ -5,7 +5,7 @@ import com.cinemamod.mcef.MCEFBrowser;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
-import net.minecraft.class_2960; // Identifier の難読化名
+import net.minecraft.util.Identifier; // class_2960 の代わりに Identifier を使用
 
 public class WebBrowserScreen extends Screen {
     private MCEFBrowser browser;
@@ -18,7 +18,6 @@ public class WebBrowserScreen extends Screen {
     protected void init() {
         super.init();
         if (this.browser == null) {
-            // MCEF.java に定義されていた正しい作成メソッド
             this.browser = MCEF.createBrowser("https://www.google.com", false);
         }
         if (this.browser != null) {
@@ -31,11 +30,10 @@ public class WebBrowserScreen extends Screen {
         super.render(context, mouseX, mouseY, delta);
         
         if (this.browser != null && this.browser.isTextureReady()) {
-            // MCEFBrowser.java の 88行目にある getTextureIdentifier() を使用
-            class_2960 texture = this.browser.getTextureIdentifier();
+            // Identifier クラスとして取得
+            Identifier texture = (Identifier) (Object) this.browser.getTextureIdentifier();
             
             if (texture != null) {
-                // 画面全体にブラウザのテクスチャを描画
                 context.drawTexture(texture, 0, 0, 0, 0, this.width, this.height, this.width, this.height);
             }
         }
@@ -43,7 +41,6 @@ public class WebBrowserScreen extends Screen {
 
     @Override
     public void close() {
-        // MCEFBrowser.java の 485行目にある close() を呼び出し
         if (this.browser != null) {
             this.browser.close();
         }
@@ -55,7 +52,7 @@ public class WebBrowserScreen extends Screen {
         return false;
     }
 
-    // マウス入力などのイベントをブラウザに送る（これがないと操作できません）
+
     @Override
     public boolean mouseClicked(double mouseX, double mouseY, int button) {
         if (this.browser != null) {
@@ -70,12 +67,5 @@ public class WebBrowserScreen extends Screen {
             this.browser.sendMouseRelease((int) mouseX, (int) mouseY, button);
         }
         return super.mouseReleased(mouseX, mouseY, button);
-    }
-
-    @Override
-    public void mouseMoved(double mouseX, double mouseY) {
-        if (this.browser != null) {
-            this.browser.sendMouseMove((int) mouseX, (int) mouseY);
-        }
     }
 }
