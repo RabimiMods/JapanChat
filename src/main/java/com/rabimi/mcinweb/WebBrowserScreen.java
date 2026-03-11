f24b1fd131d9ffd0b6b5691e833f444fadbcdfdf
RabimiYT <fakerabimi@gmail.com>
2026-03-11 15:04:17 +0900

    Update WebBrowserScreen.java

--- src/main/java/com/rabimi/mcinweb/WebBrowserScreen.java
+++ src/main/java/com/rabimi/mcinweb/WebBrowserScreen.java
@@ -1,14 +1,13 @@
 package com.rabimi.mcinweb;
 
-// クラス名を MCEFBrowser に合わせてインポート
 import com.cinemamod.mcef.MCEF;
-import com.cinemamod.mcef.MCEFBrowser; 
+import com.cinemamod.mcef.MCEFBrowser;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
+import net.minecraft.class_2960; // Identifier の難読化名
 
 public class WebBrowserScreen extends Screen {
-    // ここを Browser から MCEFBrowser に変更
     private MCEFBrowser browser;
 
     protected WebBrowserScreen() {
@@ -19,8 +18,8 @@ public class WebBrowserScreen extends Screen {
     protected void init() {
         super.init();
         if (this.browser == null) {
-            // MCEFBrowser 型として作成
-            this.browser = MCEF.BROWSER_SERVICE.createBrowser("https://www.google.com");
+            // MCEF.java に定義されていた正しい作成メソッド
+            this.browser = MCEF.createBrowser("https://www.google.com", false);
         }
         if (this.browser != null) {
             this.browser.resize(this.width, this.height);
@@ -30,15 +29,21 @@ public class WebBrowserScreen extends Screen {
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         super.render(context, mouseX, mouseY, delta);
-        if (this.browser != null) {
-            // 描画メソッド名は draw であることが多いですが、
-            // もしエラーが出る場合は render か paint か確認が必要です
-            this.browser.draw(context, mouseX, mouseY);
+        
+        if (this.browser != null && this.browser.isTextureReady()) {
+            // MCEFBrowser.java の 88行目にある getTextureIdentifier() を使用
+            class_2960 texture = this.browser.getTextureIdentifier();
+            
+            if (texture != null) {
+                // 画面全体にブラウザのテクスチャを描画
+                context.drawTexture(texture, 0, 0, 0, 0, this.width, this.height, this.width, this.height);
+            }
         }
     }
 
     @Override
     public void close() {
+        // MCEFBrowser.java の 485行目にある close() を呼び出し
         if (this.browser != null) {
             this.browser.close();
         }
@@ -48,5 +53,29 @@ public class WebBrowserScreen extends Screen {
     @Override
     public boolean shouldPause() {
         return false;
+    }
+
+    // マウス入力などのイベントをブラウザに送る（これがないと操作できません）
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        if (this.browser != null) {
+            this.browser.sendMousePress((int) mouseX, (int) mouseY, button);
+        }
+        return super.mouseClicked(mouseX, mouseY, button);
+    }
+
+    @Override
+    public boolean mouseReleased(double mouseX, double mouseY, int button) {
+        if (this.browser != null) {
+            this.browser.sendMouseRelease((int) mouseX, (int) mouseY, button);
+        }
+        return super.mouseReleased(mouseX, mouseY, button);
+    }
+
+    @Override
+    public void mouseMoved(double mouseX, double mouseY) {
+        if (this.browser != null) {
+            this.browser.sendMouseMove((int) mouseX, (int) mouseY);
+        }
     }
 }
