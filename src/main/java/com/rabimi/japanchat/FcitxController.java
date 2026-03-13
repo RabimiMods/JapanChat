c31eafb3ebae13440a1c035c98d4c6592573d526
RabimiYT <fakerabimi@gmail.com>
2026-03-13 21:21:17 +0900

    dsa

--- src/main/java/com/rabimi/japanchat/FcitxController.java
+++ src/main/java/com/rabimi/japanchat/FcitxController.java
@@ -3,20 +3,20 @@ package com.rabimi.japanchat;
 import java.io.IOException;
 
 public class FcitxController {
-
     public static void setFcitxState(boolean active) {
-        String method = active ? "FocusIn" : "FocusOut";
         try {
+            String state = active ? "2" : "1";
             new ProcessBuilder(
                 "dbus-send",
                 "--session",
                 "--dest=org.fcitx.Fcitx5",
                 "--type=method_call",
-                "/controller",
-                "org.fcitx.Fcitx.Controller1." + method
+                "/remote",
+                "org.fcitx.Fcitx.Remote1.SetCurrentIMState",
+                "int32:" + state
             ).start();
-        } catch (IOException e) {
-
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
